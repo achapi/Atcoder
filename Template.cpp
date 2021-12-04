3dd52e5cb3f46009c3363d769dc0c8babba619c7
achapi <67042875+achapi@users.noreply.github.com>
2021-12-05 01:19:17 +0900

    t

---
+++ Template.cpp
@@ -0,0 +1,23 @@
+/**
+  date : 2021年12月05日01時19分10秒
+  author : achapi
+*/
+#include <bits/stdc++.h>
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+using namespace std;
+typedef long long ll;
+
+#define endl '\n'
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+int main() {
+   cin.tie(0);cout.tie(0);
+   ios_base::sync_with_stdio(false);
+   //code start
+   
+   //code end
+   return 0;
+}
